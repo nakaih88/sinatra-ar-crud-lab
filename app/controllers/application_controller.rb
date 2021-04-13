5a5e9274eb646f95224eca5823e4c10ea998f983
Nakai <nakai.hamlin18@gmail.com>
2021-04-12 21:21:27 -0400

    commit tree 2e285dbbccd3c140d24c180b537bdc3690b0d060

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  get '/articles' do
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+      @new_article = Article.create(params)
+      redirect to "/articles/#{@new_article.id}"
+  end
+
+  get '/articles/:id' do
+   @new_article = Article.find(params[:id])
+   erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @new_article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @new_article = Article.find(params[:id])
+    @new_article.update(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@new_article.id}"
+  end
+
+
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
 end
